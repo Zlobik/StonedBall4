b3c33248761e433d837e7f420fc282e243859c62
Zlobik <60093914+Zlobik@users.noreply.github.com>
2021-01-01 13:54:59 +0300

    FirstGameVersion

---
+++ Scripts/CoinsGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsGenerator : ObjectPool
+{
+    [SerializeField] private GameObject _coinLine;
+    [SerializeField] private float _secondsBetweenSpawn = 1f;
+
+    private float _elapsedTime = 0;
+    private Vector3 _currentOffset;
+
+    private void Start ()
+    {
+        Initialize(_coinLine);
+        _currentOffset = transform.position;
+    }
+
+    private void Update ()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _secondsBetweenSpawn)
+        {
+            if (TryGetObject(out GameObject let))
+            {
+                _elapsedTime = 0;
+                Vector3 spawnPoint = new Vector3(transform.position.x + Random.Range(1, 12), transform.position.y, transform.position.z);
+                let.SetActive(true);
+                let.transform.position = spawnPoint;
+            }
+        }
+    }
+}
---
+++ Scripts/LetGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetGenerator : ObjectPool
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private float _secondsBetweenSpawn = 1f;
+
+    private float _elapsedTime = 0;
+    private Vector3 _currentOffset;
+
+    private void Start ()
+    {
+        Initialize(_template);
+        _currentOffset = transform.position;
+    }
+
+    private void Update ()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _secondsBetweenSpawn)
+        {
+            if (TryGetObject(out GameObject let))
+            {
+                _elapsedTime = 0;
+                Vector3 spawnPoint = new Vector3(transform.position.x + Random.Range(0, 10), transform.position.y, transform.position.z);
+                let.SetActive(true);
+                let.transform.position = spawnPoint;
+            }
+        }
+    }
+}
