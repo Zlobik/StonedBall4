3250fdae889fa299a8e0617a4833d7f32a7dcfab
Zlobik <60093914+Zlobik@users.noreply.github.com>
2021-02-15 23:46:58 +0300

    New actually version

---
+++ Assets/Scripts/Player/BallMover.cs
@@ -0,0 +1,43 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMover : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpDuration;
+    [SerializeField] private float _jumpHeight;
+
+    private bool _isGrounded = false;
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void Move (int speed)
+    {
+        transform.position = new Vector3(transform.position.x + (speed * _moveSpeed * Time.deltaTime), transform.position.y);
+    }
+
+    private void Update ()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Move(1);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Move(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_isGrounded)
+            {
+                transform.DOMoveY(transform.position.y + _jumpHeight, _jumpDuration);
+                _isGrounded = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPointsBlackList.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SpawnPointsBlackList : MonoBehaviour
+{
+    [SerializeField] private GameObjectsGenerator[] _gameObjectsGenerators;
+
+    private List<Vector3> _blackList = new List<Vector3>();
+
+    public event UnityAction<Vector3> AllowSpawn;
+
+    private void Start ()
+    {
+        _blackList.Add(new Vector3(-5, 5, 0));
+    }
+
+    private void OnEnable ()
+    {
+        for (int i = 0; i < _gameObjectsGenerators.Length; i++)
+        {
+            _gameObjectsGenerators[i].CheckSpawnPosition += CheckPositionToSpawn;
+            _gameObjectsGenerators[i].AddPositionToBlackList += AddPositionToBlackList;
+        }
+    }
+
+    private void OnDisable ()
+    {
+        for (int i = 0; i < _gameObjectsGenerators.Length; i++)
+        {
+            _gameObjectsGenerators[i].CheckSpawnPosition -= CheckPositionToSpawn;
+            _gameObjectsGenerators[i].AddPositionToBlackList -= AddPositionToBlackList;
+        }
+    }
+
+    private void AddPositionToBlackList (Vector3 position)
+    {
+        _blackList.Add(position);
+    }
+
+    private void CheckPositionToSpawn (Vector3 position)
+    {
+        bool isBusy = false;
+
+        for (int i = 0; i < _blackList.Count; i++)
+        {
+            if (position.x > _blackList[i].x - 1 && position.x > _blackList[i].x + 1 || position.x < _blackList[i].x - 2)
+            {
+            }
+            else
+            {
+                isBusy = true;
+            }
+        }
+
+        if (!isBusy)
+        {
+            AllowSpawn.Invoke(position);
+        }
+    }
+}
