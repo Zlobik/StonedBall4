a1a626c119d83abc2d3fe142c815fcfafcf7a743
Zlobik <60093914+Zlobik@users.noreply.github.com>
2021-03-02 17:24:20 +0300

    1
    1

---
+++ Assets/Scripts/GameObjectsGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameObjectsGenerator : MonoBehaviour
+{
+    [SerializeField] private Range _spawnRange;
+    [SerializeField] private float _secondsBetweenSpawn;
+    [SerializeField] private Pool _pool;
+    [SerializeField] private int _distanceBetweenSpawn;
+    [SerializeField] private SpawnPositionsBlackList _positionsBlackList;
+
+    private void Start ()
+    {
+        _pool.Initialize();
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn ()
+    {
+        var waitForSeconds = new WaitForSeconds(_secondsBetweenSpawn);
+        GameObject spawned = new GameObject();
+
+        while (true)
+        {
+            if (_pool.TryGetObject(out spawned))
+            {
+                Vector3 spawnPosition = GetSpawnPosition();
+
+                if (_positionsBlackList.CheckIsFreePosition(spawnPosition, _distanceBetweenSpawn))
+                {
+                    _positionsBlackList.AddPositionToBlackList(spawnPosition);
+
+                    spawned.transform.position = spawnPosition;
+                    spawned.SetActive(true);
+                }
+                else
+                {
+                    Debug.Log("Position is busy");
+                }
+            }
+
+            yield return waitForSeconds;
+        }
+    }
+
+    private Vector3 GetSpawnPosition ()
+    {
+        int spawnRange = Random.Range(_spawnRange.Min, _spawnRange.Max);
+
+        Vector3 spawnPosition = new Vector3(Convert.ToInt32(transform.position.x) + spawnRange, transform.position.y);
+
+        return spawnPosition;
+    }
+}
+
+[System.Serializable]
+public class Range
+{
+    public int Min;
+    public int Max;
+}
---
+++ Assets/Scripts/MainMenu/CreatorsButton.cs
@@ -0,0 +1,33 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatorsButton : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _creatorsPanel;
+    [SerializeField] private Button _backToMenu;
+    [SerializeField] private float _fadeTime;
+
+    public void OnButtonClick ()
+    {
+        _creatorsPanel.gameObject.SetActive(true);
+        _creatorsPanel.DOFade(1, _fadeTime);
+    }
+
+    public void OnBackButtonClick ()
+    {
+        _creatorsPanel.DOFade(0, _fadeTime);
+
+        StartCoroutine(DisablePanel());
+    }
+
+    private IEnumerator DisablePanel ()
+    {
+        var waitForSeconds = new WaitForSeconds(_fadeTime);
+
+        yield return waitForSeconds;
+
+        _creatorsPanel.gameObject.SetActive(false);
+    }
+}
