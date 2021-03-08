2bed8f492af31d2da885bfd2724552b977a164c4
Zlobik <60093914+Zlobik@users.noreply.github.com>
2021-03-08 12:07:39 +0300

    New
    version

---
+++ Assets/Scripts/GameObjectsGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameObjectsGenerator : MonoBehaviour
+{
+    [SerializeField] private Range _spawnRange;
+    [SerializeField] private float _secondsBetweenSpawn;
+    [SerializeField] private Pool _pool;
+    [SerializeField] private Map _positionsBlackList;
+
+    private bool _spawnerWork = true;
+
+    private void Start ()
+    {
+        _pool.Initialize();
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn ()
+    {
+        var timeBeforeRespawn = new WaitForSeconds(_secondsBetweenSpawn);
+
+        while (_spawnerWork)
+        {
+            if (_pool.TryGetObject(out GameObject spawned))
+            {
+                Vector3Int spawnPosition = _spawnRange.GenerateSpawnPosition(transform.position);
+
+                if (_positionsBlackList.IsFree(spawnPosition))
+                {
+                    spawned.transform.position = spawnPosition;
+                    _positionsBlackList.Add(spawned.transform);
+                    spawned.SetActive(true);
+                }
+            }
+            else
+            {
+                _spawnerWork = false;
+            }
+
+            yield return timeBeforeRespawn;
+        }
+    }
+}
+
+[System.Serializable]
+public class Range
+{
+    public int Min;
+    public int Max;
+
+    public Vector3Int GenerateSpawnPosition (Vector3 currentPosition)
+    {
+        int spawnRange = Random.Range(Min, Max);
+
+        return new Vector3Int(Convert.ToInt32(currentPosition.x) + spawnRange, Convert.ToInt32(currentPosition.y), 0);
+    }
+}
---
+++ Assets/Scripts/Player/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Ball : MonoBehaviour
+{
+    private int _coins = 0;
+
+    public event UnityAction<int> OnCoinCollected;
+
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.GetComponent<Coin>())
+        {
+            _coins++;
+            OnCoinCollected?.Invoke(_coins);
+        }
+    }
+}
