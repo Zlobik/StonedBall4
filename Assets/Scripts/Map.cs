d1ba9f3a5ec94c6b8cb01fec34627377371aaf68
Zlobik <60093914+Zlobik@users.noreply.github.com>
2021-03-11 17:42:43 +0300

    1
    2

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    private List<Transform> _objects = new List<Transform>();
+
+    public void Add (Transform @object)
+    {
+        _objects.Add(@object);
+    }
+
+    public bool IsFree (ref Vector3 position)
+    {
+        position = new Vector3Int(Convert.ToInt32(position.x), Convert.ToInt32(position.y), 0);
+
+        int xPosition = Convert.ToInt32(position.x);
+
+        foreach (var bannedTransform in _objects)
+        {
+            if (xPosition == bannedTransform.position.x)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Pool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Pool : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _templates;
+    [SerializeField] private int _poolCapacity;
+
+    private List<GameObject> _pool = new List<GameObject>();
+
+    public void Initialize ()
+    {
+        for (int i = 0; i < _poolCapacity; i++)
+        {
+            GameObject spawned = Instantiate(_templates[Random.Range(0, _templates.Length)], transform);
+            spawned.SetActive(false);
+            _pool.Add(spawned);
+        }
+    }
+
+    public bool TryGetObject (out GameObject result)
+    {
+        result = _pool.FirstOrDefault(p => p.activeSelf == false);
+
+        return result != null;
+    }
+
+    public void ResetPool ()
+    {
+        foreach (var item in _pool)
+            item.SetActive(false);
+    }
+}
